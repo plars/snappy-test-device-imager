b682681871856424ac8fdb8eb541d68aa36df2d3
Paul Larson <paul.larson@canonical.com>
2016-01-14 16:59:30 -0600

    Split up the handlers

--- snappy-test-device-imager/main.go
+++ snappy-test-device-imager/main.go
@@ -33,7 +33,8 @@ func main() {
 }
 
 func run() error {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/writeimage", writeimage)
+	http.HandleFunc("/reboot", reboot)
 	ch := make(chan error, 2)
 	go func() {
 		ch <- http.ListenAndServe(":8989", nil)
@@ -41,39 +42,38 @@ func run() error {
 	return <-ch
 }
 
-func handler(resp http.ResponseWriter, req *http.Request) {
-	if req.URL.Path == "/writeimage" {
-		server_arg := req.URL.Query().Get("server")
-		s := strings.Split(server_arg, ":")
-		if len(s) != 2 {
-			http.Error(resp, "ERROR: Specify server as IP:PORT",
-				http.StatusBadRequest)
-			return
-		}
-		ip, port := s[0], s[1]
-		dev := req.URL.Query().Get("dev")
-		if _, err := os.Stat(dev); os.IsNotExist(err) {
-			http.Error(resp, "ERROR: block device: " + dev + " does not exist", http.StatusBadRequest)
-			return
-		}
-		ddcmd := fmt.Sprintf("nc %v %v |gunzip|dd bs=16777216 of=%v", ip, port, dev)
-		cmd := "/bin/sh"
-		args := []string{"-c", ddcmd}
-		output, err := exec.Command(cmd, args...).CombinedOutput()
-		if err != nil {
-			http.Error(resp, string(output), http.StatusBadRequest)
-			return
-		}
-		// Just do a blind call to sync
-		exec.Command("/bin/sync").Run()
-		resp.Write([]byte(string(output)))
+func writeimage(resp http.ResponseWriter, req *http.Request) {
+	server_arg := req.URL.Query().Get("server")
+	s := strings.Split(server_arg, ":")
+	if len(s) != 2 {
+		http.Error(resp, "ERROR: Specify server as IP:PORT",
+			http.StatusBadRequest)
 		return
 	}
-	if req.URL.Path == "/reboot" {
-		output, err := exec.Command("/bin/reboot").CombinedOutput()
-		if err != nil {
-			http.Error(resp, string(output), http.StatusBadRequest)
-			return
-		}
+	ip, port := s[0], s[1]
+	dev := req.URL.Query().Get("dev")
+	if _, err := os.Stat(dev); os.IsNotExist(err) {
+		http.Error(resp, "ERROR: block device: " + dev + " does not exist", http.StatusBadRequest)
+		return
+	}
+	ddcmd := fmt.Sprintf("nc %v %v |gunzip|dd bs=16777216 of=%v", ip, port, dev)
+	cmd := "/bin/sh"
+	args := []string{"-c", ddcmd}
+	output, err := exec.Command(cmd, args...).CombinedOutput()
+	if err != nil {
+		http.Error(resp, string(output), http.StatusBadRequest)
+		return
+	}
+	// Just do a blind call to sync
+	exec.Command("/bin/sync").Run()
+	resp.Write([]byte(string(output)))
+	return
+}
+
+func reboot(resp http.ResponseWriter, req *http.Request) {
+	output, err := exec.Command("/bin/reboot").CombinedOutput()
+	if err != nil {
+		http.Error(resp, string(output), http.StatusBadRequest)
+		return
 	}
 }
